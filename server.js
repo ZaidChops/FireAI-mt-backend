10b222dcdf31507b082c7aed62ceb625934b6de2
ZaidChops <jaidhussainz@gmail.com>
2024-04-29 00:39:13 +0530

    2nd commit

--- server.js
+++ server.js
@@ -2,12 +2,25 @@ const express = require('express')
 const colors = require('colors');
 const { connectDB } = require('./Config/db-config');
 const dotenv = require('dotenv').config()
+const cors = require("cors");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 // DB CONNECTION
 connectDB()
+
+// CORS
+app.use(
+    cors({
+      origin: `${process.env.FRONTEND_URL}`,
+      methods: "GET,PUT,POST,DELETE",
+      credentials: true,
+    })
+  );
+
+// Handle preflight requests
+app.options(`${process.env.FRONTEND_URL}`, cors());
 
 // BODY PARCER
 app.use(express.json())
