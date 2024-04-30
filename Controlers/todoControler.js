e4262c0055ca06313bb1d2126899434ad1361925
ZaidChops <jaidhussainz@gmail.com>
2024-04-30 15:45:09 +0530

    4th commit

--- Controlers/todoControler.js
+++ Controlers/todoControler.js
@@ -1,65 +1,70 @@
-const Todo = require("../Models/todo-Model")
-
+const Todo = require("../Models/todo-Model");
 
 // GET-ALL
-const getTodos = async (req,res) =>{
-    const allTodos = await Todo.find()
-    res.json(allTodos)
-}
+const getTodos = async (req, res) => {
+  const allTodos = await Todo.find();
+  res.json(allTodos);
+};
 
 // CREATE
-const createTodo = async (req,res) =>{
-   const {title, description, isCompleted} = req.body
-    // console.log("req", req.body)
-   if(!title || !description){
-    res.status(400)
-    throw new Error("Please fill all details".red)
-   }
+const createTodo = async (req, res) => {
+  const { title, description, isCompleted } = req.body;
+  console.log("req", req.body);
+  if (!title || !description) {
+    res.status(400);
+    throw new Error("Please fill all details".red);
+  }
 
-   const newTodo = await Todo.create({
-    userId: req.params.userId ,
+  const newTodo = await Todo.create({
+    userId: req.params.userId,
     title,
     description,
-    isCompleted
-   })
-//    console.log("object", newTodo);
-   if(newTodo){
-    res.status(200).json(newTodo)
-   }
-   else{
-    throw new Error("Something went wrong".red)
-   }
-}
+    isCompleted,
+  });
+  //    console.log("object", newTodo);
+  if (newTodo) {
+    res.status(200).json(newTodo);
+  } else {
+    throw new Error("Something went wrong".red);
+  }
+};
 
 // GET-SINGLE
-const getSingleTodo = async (req,res)=>{
-    const singleTodo = await Todo.findById(req.params.id)
+const getSingleTodo = async (req, res) => {
+  const singleTodo = await Todo.findById(req.params.id);
 
-    if(!singleTodo){
-        res.status(404)
-        throw new Error("Todo not found")
-    }
-    res.status(200).json(singleTodo)
-}
+  if (!singleTodo) {
+    res.status(404);
+    throw new Error("Todo not found");
+  }
+  res.status(200).json(singleTodo);
+};
 
 // DELETE
-const deleteTodo = async (req,res) =>{
-    await Todo.findByIdAndDelete(req.params.id)
-    res.status(200).json({
-        msg: "Todo Deleted"
-    })
-}
+const deleteTodo = async (req, res) => {
+  await Todo.findByIdAndDelete(req.params.id);
+  const allTodos = await Todo.find();
+  res.json(allTodos);
+};
 
 // UPDATE
-const updateTodo = async (req,res) =>{
-   const updated = await Todo.findByIdAndUpdate(req.params.id, req.body, {new:true})
+const updateTodo = async (req, res) => {
+  const updated = await Todo.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+  });
 
-   if(!updated){
-    res.status(404)
-    throw new Error("Something went wrong")
-   }
-   res.status(200).json(updated)
+  if (!updated) {
+    res.status(404);
+    throw new Error("Something went wrong");
+  }
+  const allTodos = await Todo.find();
+  res.json(allTodos);
+};
 
-}
-
-module.exports = {getTodos, createTodo, getSingleTodo, deleteTodo, updateTodo}
+module.exports = {
+  getTodos,
+  createTodo,
+  getSingleTodo,
+  deleteTodo,
+  updateTodo,
+};
